09edd3367391df55a2ef8a4dbf4b5c5a27c8eaf6
oldDong <2232890213@qq.com>
2020-08-11 11:14:57 +0800

    update-抽离css可缓存

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title:'东芝',
-        template: './src/assets/index.html'
-    })],
-    module: {
-        rules: [
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '东芝',
+      template: './src/assets/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // 类似 webpackOptions.output里面的配置 可以忽略
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+
+        // use: ['style-loader', 'css-loader'],
+        use: [
           {
-            test: /\.css$/i,
-            use: [ 'style-loader', 'css-loader' ]
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
           },
+          'css-loader',
         ],
       },
+    ],
+  },
 
 };
