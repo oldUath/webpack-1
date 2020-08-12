b30cfefcdf69060f7862fe08e7a26be4a9fdca69
oldDong <2232890213@qq.com>
2020-08-12 12:02:28 +0800

    懒加载-更新了index.js和lazy.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import image from './assets/yaya.jpg'
 console.log(image)
 const div = document.querySelector('#app')
 div.innerHTML=`<img src='${image}'>`
+
+const button = document.createElement('button')
+button.innerText="懒加载"
+button.onclick=()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{})   
+}
+
+div.appendChild(button)
---
+++ src/lazy.js
@@ -0,0 +1,3 @@
+export default function lazy(){
+    console.log('我是一懒加载模块')
+}
